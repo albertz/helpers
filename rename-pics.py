97c9ce917a6302bb2660562eb01b24e79345b92f
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2011-01-23 21:48:01 +0100

    rename-pics finished

--- rename-pics.py
+++ rename-pics.py
@@ -29,22 +29,78 @@ def cleanup_exif_tags(exif):
 		ret[tag] = value
 	return ret
 
-def iminfo(f):
-	return cleanup_exif_tags(exif.getexif(f))
+def file_time_creation(f):
+	import os, time
+	stats = os.stat(f)
+	try:
+		t = stats.st_birthtime
+	except:
+		t = stats.st_ctime
+	return time.strftime("%Y:%m:%d %H:%M:%S",time.localtime(t))
 	
-allexif = {}
-for f in recglob(dir + "/*.{jpeg,jpg,JPG}"):
-	print f, ":",
+def iminfo(f):
+	try:
+		info = cleanup_exif_tags(exif.getexif(f))
+	except:
+		info = {}
+	if not "DateTime" in info:
+		info["DateTime"] = file_time_creation(f)
+	return info
+
+def user_input(text, convfunc):
+	while True:
+		try:
+			s = raw_input(text)
+			return convfunc(s)
+		except Exception, e:
+			print "Error:", e
+
+def str_to_bool(s):
+	if s.lower() in ["y", "yes", "ja", "j", "1"]: return True
+	if s.lower() in ["n", "no", "nein", "0"]: return False
+	raise Exception, "I don't understand '" + s + "'; please give me an Y or N"
+	
+files = {}
+errors = {}
+
+for f in recglob(dir + "/*.{jpeg,jpg,JPG,mov,MOV,png,PNG}"):
 	try:
 		info = iminfo(f)
-		if "DateTime" in info:
-			print info["DateTime"]
+		prefix = info["DateTime"].replace(":", "_")[0:10] # get eg. "2011_01_22"
+		prefix += "__"
+		newfn = os.path.dirname(f) + "/" + prefix + os.path.basename(f)
+		if os.path.exists(newfn):
+			errors[f] = os.path.basename(newfn) + " already exists"
+		elif os.path.basename(f)[0:len(prefix)] == prefix:
+			errors[f] = os.path.basename(f) + " already has the prefix '" + prefix + "'"			
 		else:
-			print "incomplete EXIF"
-		allexif.update(info)
+			files[f] = newfn
 	except Exception, e:
-		print e
+		errors[f] = str(e)
 
-for tag, value in allexif.iteritems():
-	print tag, ":", value
+while True:
+	if len(files) > 0:
+		print "Renames:"
+		for old, new in sorted(files.items()):
+			print "", old, "->", os.path.basename(new)
+		print ""
 	
+	if len(errors) > 0:
+		print "Errors (i.e. excluded files):"
+		for f, err in errors.iteritems():
+			print "", f, ":", err
+		print ""
+	
+	if len(files) == 0:
+		print "No files to rename. Quitting."
+		quit()
+	
+	ok = user_input("Confirm? (Y/N) ", str_to_bool)
+	if ok:
+		for old, new in sorted(files.items()):
+			os.rename(old, new)
+		print "All renames successfull."
+		quit()
+	else:
+		print "Abborting."
+		quit()
