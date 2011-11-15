874fda28917233c28ceffdc5bd89772e66029820
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2011-11-15 13:34:14 +0100

    28c3 script

--- 28c3.py
+++ 28c3.py
@@ -1,11 +1,19 @@
 import requests
 from BeautifulSoup import BeautifulSoup
 from time import sleep
+import os, sys, traceback
 
-s = requests.session()
+secrets = eval(open(os.path.expanduser("~/Documents/secrets")).read())
+username = secrets["28c3_username"]
+password = secrets["28c3_password"]
 
-username = ''
-password = ''
+s = requests.session(
+    headers = {
+        "Origin": "https://presale.events.ccc.de/",
+        "User-Agent": "Mozilla/5.0",
+        "Accept-Charset": "ISO-8859-1,utf-8;q=0.7,*;q=0.3",
+    }
+)
 
 print "CCC-Presale 28C3 Script by @sctan [https://github.com/szechuen/CCC-Presale]"
 
@@ -14,30 +22,72 @@ print "CCC-Presale 28C3 Script by @sctan [https://github.com/szechuen/CCC-Presal
 print "Accessing Sign In..."
 
 while True:
-    sign_in = s.get("https://presale.events.ccc.de/accounts/sign_in")
-    if sign_in.ok: break
-    print "Request Error - Retrying..."
+    try:
+        sign_in = s.get("https://presale.events.ccc.de/accounts/sign_in")
+        if sign_in.ok: break
+    except KeyboardInterrupt:
+        quit()
+    except:
+        traceback.print_exc()
+    print "Request Error - Retrying getting sign in page..."
 
+print "sign_in page url:", sign_in.url
 sign_in_soup = BeautifulSoup(sign_in.content)
 
-account_post = {}
-for item in sign_in_soup.findAll("input", type="hidden"): account_post[item['name']] = item['value']
-for item in sign_in_soup.findAll("input", type="submit"): account_post[item['name']] = item['value']
-account_post['account[username]'] = username
-account_post['account[password]'] = password
+account_post = []
+for item in sign_in_soup.findAll("input", type="hidden"):
+    print "hidden", item
+    account_post.append((item['name'], item['value']))
+account_post.append(('account[username]', username))
+account_post.append(('account[password]', password))
+for item in sign_in_soup.findAll("input", type="submit"):
+    print "submit", item
+    account_post.append((item['name'], item['value']))
 
 account_link = "https://presale.events.ccc.de" + sign_in_soup.find("form")['action']
+print "account link:", account_link
+print s.cookies
+print account_post
+
+class Foo: pass
+account_p = Foo()
+account_p.items = lambda: account_post
 
 # Account
 
 print "Accessing Account..."
 
 while True:
-    account = s.post(account_link, data=account_post)
-    if account.ok: break
-    print "Request Error - Retrying..."
-
-account_soup = BeautifulSoup(account.content)
+    try:
+        account = s.post(account_link, data=account_p,
+                         headers = {
+                            "Referer": sign_in.url,
+                         },
+                         #return_response=False
+                         )
+        #account.send()
+        #print account, account._enc_data, account.headers
+        #account = account.response
+        if account.status_code == 302:
+            s.cookies.update(account.cookies)
+            print "redirect..."
+            account = s.get("https://presale.events.ccc.de/order/")
+        print account
+        if account.ok:
+            open("last_login.html", "w").write(account.content.encode("utf8"))
+            account_soup = BeautifulSoup(account.content)
+            if account_soup.find(text=lambda(x): x.find("Signed in successfully") != -1):
+                break
+            else:
+                print "Could not login!"
+                os._exit(1)
+                sleep(0.5)
+        print account
+    except KeyboardInterrupt:
+        quit()
+    except:
+        traceback.print_exc()        
+    print "Request Error - Retrying login..."
 
 ordered = False
 page = account
@@ -50,11 +100,20 @@ while not ordered:
     for item in soup.findAll("input", type="submit"): post[item['name']] = item['value']
 
     link = "https://presale.events.ccc.de" + soup.find("form")['action']
-
+    print "Ordering ...", link
     while True:
-        page = s.post(link, data=post)
-        if page.ok: break
-        print "Request Error - Retrying..."
+        try:
+            page = s.post(link, data=post)
+            if page.status_code == 302:
+                print "redirect..."
+                page = s.get("https://presale.events.ccc.de/order/")
+            print page
+            if page.ok: break
+        except KeyboardInterrupt:
+            quit()
+        except:
+            traceback.print_exc()
+        print "Request Error - Retrying to order..."
     
     soup = BeautifulSoup(page.content)
 
@@ -64,6 +123,9 @@ while not ordered:
 
     if soup.find(text=lambda(x): x.find("There are currently not enough tickets available") != -1): 
         print "Not Open - Sleeping before retrying..."
+        sleep(1)
+    elif soup.find(text=lambda(x): x.find("Username (this is NOT your email address)") != -1 or x.find("Username (this is NOT your email address)") != -1):
+        print "Error, not logged in anymore?"
         sleep(1)
     else:
         print "Ordered :D"
