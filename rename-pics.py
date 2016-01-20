38534e1b713b504a65637e201bf417cfcd4b2635
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2016-01-20 07:38:10 +0100

    rename pics: some restructuring

--- rename-pics.py
+++ rename-pics.py
@@ -8,28 +8,15 @@
 # Asks for confirmation, so it is safe to just try out and see what it would do.
 
 
+import argparse
 from glob import *
 from recglob import *
 from cleanupstr import *
+import exif
 import sys, os
 import better_exchook
 better_exchook.install()
 
-def usage():
-	print "usage:", sys.argv[0], "<dir>"
-	print "Asks for confirmation before it does any action."
-	
-if len(sys.argv) != 2:
-	usage()
-	quit()
-
-dir = sys.argv[1]
-if not os.path.isdir(dir):
-	print dir, "is not a directory"
-	usage()
-	quit()
-
-import exif
 
 def cleanup_exif_tags(exif):
 	ret = {}
@@ -48,7 +35,7 @@ def file_time_creation(f):
 		t = stats.st_birthtime
 	except Exception:
 		t = stats.st_ctime
-	return time.strftime("%Y:%m:%d %H:%M:%S",time.localtime(t))
+	return time.strftime("%Y:%m:%d %H:%M:%S", time.localtime(t))
 	
 def iminfo(f):
 	try:
@@ -71,48 +58,107 @@ def str_to_bool(s):
 	if s.lower() in ["y", "yes", "ja", "j", "1"]: return True
 	if s.lower() in ["n", "no", "nein", "0"]: return False
 	raise Exception, "I don't understand '" + s + "'; please give me an Y or N"
-	
+
+
 files = {}
 errors = {}
 
-for f in recglob(dir + "/*.{jpeg,jpg,JPG,mov,MOV,png,PNG}"):
+def get_prefix_for_file(f, args):
+	info = iminfo(f)
+	date_time_str = info["DateTime"].replace(":", "_").replace(" ", "_")
+	date_time_plen = 10  # prefix, eg. "2011_01_22"
+	if args.add_time:
+		date_time_plen += 9  # e.g. "_12_30_00"
+	prefix = date_time_str[:date_time_plen]
+	prefix += "__"
+	return prefix
+
+def collect_file(f, args):
+	global files, errors
 	try:
-		info = iminfo(f)
-		prefix = info["DateTime"].replace(":", "_")[0:10] # get eg. "2011_01_22"
-		prefix += "__"
+		assert os.path.isfile(f), "is not a file: %s" % f
+		prefix = get_prefix_for_file(f, args)
 		newfn = os.path.dirname(f) + "/" + prefix + os.path.basename(f)
 		if os.path.exists(newfn):
 			errors[f] = os.path.basename(newfn) + " already exists"
 		elif os.path.basename(f)[0:len(prefix)] == prefix:
-			errors[f] = os.path.basename(f) + " already has the prefix '" + prefix + "'"			
+			errors[f] = os.path.basename(f) + " already has the prefix '" + prefix + "'"
 		else:
 			files[f] = newfn
 	except Exception, e:
 		errors[f] = str(e)
 
-while True:
-	if len(files) > 0:
-		print "Renames:"
-		for old, new in sorted(files.items()):
-			print "", old, "->", os.path.basename(new)
-		print ""
-	
-	if len(errors) > 0:
-		print "Errors (i.e. excluded files):"
-		for f, err in errors.iteritems():
-			print "", f, ":", err
-		print ""
-	
-	if len(files) == 0:
-		print "No files to rename. Quitting."
-		quit()
-	
-	ok = user_input("Confirm? (Y/N) ", str_to_bool)
-	if ok:
-		for old, new in sorted(files.items()):
-			os.rename(old, new)
-		print "All renames successfull."
-		quit()
-	else:
-		print "Abborting."
+
+def collect_dir(dir, args):
+	for f in recglob(dir + "/*.{jpeg,jpg,JPG,mov,MOV,png,PNG}"):
+		collect_file(f, args)
+
+
+def collect(fn, args):
+	if os.path.isdir(fn):
+		return collect_dir(fn, args)
+	if os.path.isfile(fn):
+		return collect_file(fn, args)
+	raise Exception("no file or dir: %s" % fn)
+
+
+def user_loop(args):
+	while True:
+		if len(files) > 0:
+			print "Renames:"
+			for old, new in sorted(files.items()):
+				print "", old, "->", os.path.basename(new)
+			print ""
+
+		if len(errors) > 0:
+			print "Errors (i.e. excluded files):"
+			for f, err in errors.iteritems():
+				print "", f, ":", err
+			print ""
+
+		if len(files) == 0:
+			print "No files to rename. Quitting."
+			quit()
+
+		if args.no_action:
+			print "No action (--no_action). Quitting."
+			quit()
+
+		ok = user_input("Confirm? (Y/N) ", str_to_bool)
+		if ok:
+			for old, new in sorted(files.items()):
+				os.rename(old, new)
+			print "All renames successfull."
+			quit()
+		else:
+			print "Abborting."
+			quit()
+
+
+def main():
+	argparser = argparse.ArgumentParser(
+		description="Renames pictures according to a pattern via EXIF.",
+		epilog="Asks for confirmation before it does any action."
+	)
+	argparser.add_argument(
+		'dirs_or_files', nargs="+",
+		help="dirs/files to proceed")
+	argparser.add_argument(
+		'--add_time', action="store_true",
+		help="add time to filename")
+	argparser.add_argument(
+		'--no_action', action="store_true",
+		help="just try, don't do anything")
+	args = argparser.parse_args()
+
+	for fn in args.dirs_or_files:
+		collect(fn, args)
+
+	user_loop(args)
+
+if __name__ == "__main__":
+	try:
+		main()
+	except KeyboardInterrupt:
+		print "KeyboardInterrupt. Abborting."
 		quit()
