284b8384d7d865a934616357ff467281fa472c37
Albert Zeyer <albzey@gmail.com>
2021-03-21 18:10:31 +0100

    find related Git commits, more

--- git-find-related-commits.py
+++ git-find-related-commits.py
@@ -36,32 +36,34 @@ class GitHelper:
     """
     return list(reversed(list(self.repo.iter_commits(f"{self.get_base_commit()}..{self.local_branch}"))))
 
-  def score_commit_pair_squash(self, commits: List[git.Commit]) -> Tuple[Optional[int], List[str]]:
+  def score_commit_pair_squash(self, commits: List[git.Commit]) -> Tuple[Optional[int], Optional[int], List[str]]:
     commit0 = commits[0]
     assert len(commit0.parents) == 1  # not implemented otherwise...
     commit0 = commit0.parents[0]
     # print(f"Start at {_format_commit(commit0)}")
     diffs = []
     diff_counts = []
+    rel_diff_c = None
     with self.in_tmp_branch(commit0):
       for commit in commits:
         # print(f"Apply {_format_commit(commit)}")
         try:
           self.repo.git.cherry_pick(commit, "--keep-redundant-commits")
         except git.GitCommandError:
-          return None, diffs
+          return None, None, diffs
         diff_str = self.repo.git.diff(f"{commit0}..HEAD")
         c = _count_changed_lines(diff_str)
         if diff_counts:
           commit_diff_str = self.repo.git.show(commit)
           cc = _count_changed_lines(commit_diff_str)
           rel_diff = c - diff_counts[-1]
-          if rel_diff >= cc:  # this commit has no influence on the prev commit. so skip this whole proposed squash
-            return None, diffs
+          rel_diff_c = rel_diff - cc
+          if rel_diff_c >= 0:  # this commit has no influence on the prev commit. so skip this whole proposed squash
+            return None, None, diffs
         diffs.append(diff_str)
         diff_counts.append(c)
     rel_diff = diff_counts[-1] - diff_counts[0]
-    return rel_diff, diffs
+    return rel_diff, rel_diff_c, diffs
 
   def test(self):
     commits = self.get_commit_list()
@@ -73,16 +75,15 @@ class GitHelper:
     for i in range(len(commits)):
       for j in range(i + 1, len(commits)):
         commit_pair = [commits[i], commits[j]]
-        c, diffs = self.score_commit_pair_squash(commit_pair)
+        c, c_, diffs = self.score_commit_pair_squash(commit_pair)
         print("Commits:", [_format_commit(commit) for commit in commit_pair], "relative diff count:", c)
         if c is not None:
-          results.append((c, commit_pair, diffs))
+          results.append((c_, c, commit_pair, diffs))
 
     print("Done. Results:")
     results.sort(key=lambda x: x[0])
-    for c, commit_pair, diffs in results:
-      print("***", c, "commits:", [_format_commit(commit) for commit in commit_pair])
-      assert len(commit_pair) == len(diffs)
+    for c_, c, commit_pair, diffs in results:
+      print("***", c_, c, "commits:", [_format_commit(commit) for commit in commit_pair])
 
   @contextlib.contextmanager
   def in_tmp_branch(self, commit: git.Commit) -> git.Head:
